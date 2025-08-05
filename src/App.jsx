31afb52968f5d7c78f049fdd74021d9a236aeeb8
Nischal Puri <mrnischalpuri@gmail.com>
2025-08-05 18:54:55 +0545

    scroll smoother pugin added

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import gsap from "gsap"
 import NavBar from "./components/NavBar"
 import HeroSection from "./sections/HeroSection"
-import { ScrollTrigger } from "gsap/all"
+import { ScrollSmoother, ScrollTrigger } from "gsap/all"
 import MessageSection from "./sections/MessageSection"
 import FlavorSection from "./sections/FlavorSection"
 
 // register ScrollTrigger
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
 function App() {
   return (
     <>
-      <NavBar />
-      <HeroSection />
-      <MessageSection />
-      <FlavorSection />
-      <div className="h-dvh border"></div>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <NavBar />
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <div className="h-dvh border"></div>
+        </div>
+      </div>
     </>
   )
 }
